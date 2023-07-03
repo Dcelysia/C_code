7b6f9d74306b0f2b3012cb2f83a622aee36c8031
jianyu12311 <6873058@qq.com>
2023-07-03 10:27:19 +0800

    text

---
+++ 6.30 1/6.30 1/源.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+//int main()
+//{
+//	printf("上课（1/0）");
+//	int input = 0;
+//
+//	scanf_s("%d", &input);
+//
+//
+//	if (input == 1)
+//		printf("1");
+//	else
+//		printf("2");
+//
+//	return 0;
+//}
+
+
+////int main(void)
+//{
+//	int line = 0;
+//	printf("玩游戏\n");
+//	while (line < 3000)
+//	{
+//		printf("打游戏\n,%d",line);
+//		line++;
+//	}
+//	if (line = 3000)
+//	{
+//		printf("win");
+//	}
+//
+//
+//	return 0;
+//
+//
+//Add(int x, int y)
+//{
+//	int z;
+//	z = x + y;
+//	return z;
+//}
+
+//
+//
+//int main()
+//{
+//	int sum1=0;
+//	int sum2=0;
+//	scanf_s("%d%d", &sum1, &sum2);
+//	int sum = Add(sum1, sum2);
+//	
+//	printf("和是%d\n", sum);
+//
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10};
+//	while (i < 10)
+//	{
+//		printf("%d\n", arr[i]);
+//		i++;
+//	}
+//	
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//	int a = 9 %2;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 2;
+//	int b = a <<1;
+//	printf("%d\n", b);
+//	printf("%d\n", !a);
+//
+////	return 0;
+//
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", sizeof(int));
+//
+//	return 0;
+//}
+
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));
+
+
+
+	
+	return 0;
+}
