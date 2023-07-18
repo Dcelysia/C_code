5d16f467fa0f99f73f706cc499dc6c661802b1aa
jianyu12311 <6873058@qq.com>
2023-07-18 21:08:38 +0800

    practice

---
+++ 2023 7.18/2023 7.18/practice.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	printf("hehe\n");
+//	main();
+//
+//	return 0;
+//}
+//void print(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		print(num / 10);
+//	}
+//	printf("%d ", num % 10);
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%u",&num);
+//	print(num);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = "hello";
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//
+//}
+int my_strlen(char* str)
+{
+
+}
+int main()
+{
+	char arr[] = "hello";
+	printf("%d\n",my_strlen(arr));
+	return 0;
+
+}
