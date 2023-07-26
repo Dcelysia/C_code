2f33f007d244923447c44369690950e220264eeb
jianyu12311 <6873058@qq.com>
2023-07-26 22:25:21 +0800

    practice

---
+++ 2023 7_26/2023 7_26/practice.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+//int main()
+//{
+//	//float i = 6.0f / 5.0f;
+//	double i = 6.0 / 5.0;
+//	printf("%lf", i);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int b = a << 1;
+//	printf("b=%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a & b;
+//	int d = a | b;
+//	int i = a ^ b;
+//	printf("c=%d\nd=%d\ni=%d\n", c,d,i);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 13;
+//	int b = 1;
+//	int c = a & b;
+//	int i = 0;
+//	int output = 0;
+//    for(i=0;i<32;i++)
+//	{
+//		int c = a & b;
+//		if (c == 1)
+//		{
+//			output++;
+//		}
+//		a = a >> 1;
+//	}
+//	printf("%d\n", output);
+//	printf("%d\n", c);
+//	return 0;
+//}
