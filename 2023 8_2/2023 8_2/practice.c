a7f980b0c122de365186370ca3d98371ef77f87e
jianyu12311 <6873058@qq.com>
2023-08-02 21:49:08 +0800

    practice.c

---
+++ 2023 8_2/2023 8_2/practice.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <assert.h>
+//void my_strcpy(char* dest,const char* src)
+//{
+//    assert(src != NULL);
+//    int* ret=
+//    while (*dest++ = *src++);
+//}
+//int main(void)
+//{
+//    char arr1[10] = "*********";
+//    char arr2[10] = "hello";
+//    my_strcpy(arr1, arr2);
+//    printf("%s\n", arr1);
+//    return 0;
+//}
+//int main()
+//{
+//	const int p = 2;
+//	int* a = &p;
+//	*a = 20;
+//	printf("%d\n", p);
+//	return 0;
+//}
+//int main()
+//{
+//	const int p = 2;
+//	const int* a = &p;
+//	*a = 20;
+//	printf("%d\n", p);
+//	return 0;
+//}int main()
+//int  main()
+//{
+//	int text = 4;
+//	int p = 5;
+//	int* const pa = text;
+//	/*pa = &text;*/
+//	*pa = 55;
+//	return 0;
+//}
+//unsigned int my_strlen(char* arr)
+//{
+//	unsigned int i = 0;
+//	while (*arr++ != '\0') i++;
+//	return i;
+//}
+//int main()
+//{
+//	char arr[10] = "abcdef" ;
+//	printf("%u\n",my_strlen(arr));
+//	return 0;
+//}
