bdf90ee0080fbf23876aba58eb156cb9373ae49c
jianyu12311 <6873058@qq.com>
2023-08-04 22:09:45 +0800

    practice.c

---
+++ 2023 8_4/2023 8_4/practice.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//求两个数最小公倍数的函数
+
+//int main()
+//{
+//	int input1 = 0;
+//	int input2 = 0;
+//	scanf("%d %d", &input1,&input2);
+//	int tem = 0;
+//	if (input1 < input2)
+//	{
+//		int tem = 0;
+//		tem = input1;
+//		input1 = input2;
+//		input2 = tem;
+//		
+//	}
+//	int ret = input2;
+//	int i = 0;
+//	for (i = 1; ; i++)
+//	{
+//		ret = input2 * i;
+//		if (ret % input1 == 0)
+//			break;
+//	}
+//
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//将一句话中的单词倒过来
