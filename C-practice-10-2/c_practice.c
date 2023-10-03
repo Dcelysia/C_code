4136ee21b5073b906a32d68c9894cd1cc0b0c981
Elysia <6873058@qq.com>
2023-10-03 21:37:15 +0800

    daily practice

---
+++ C-practice-10-2/c_practice.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int maxProfit(int* prices, int pricesSize) {
+//    int max = 0;
+//    for (int i = 0; i < pricesSize; i++)
+//    {
+//        for (int j = 1; j < pricesSize - i; j++)
+//        {
+//            if (prices[i] < prices[i+j])
+//            {
+//                int sum = prices[i + j] - prices[i];
+//                max = max > sum ? max : sum;
+//            }
+//        }
+//    }
+//    if (max != 0)
+//    {
+//        return max;
+//    }
+//    else
+//    {
+//        return 0;
+//    }
+//}
+//
+//int main(void)
+//{
+//    int arr1[] = { 7,6,5,4,3,2,1 };
+//    int size = 7;
+//    maxProfit(arr1, size);
+//    return 0;
+//}
+//¸ÄÉÆ°æ±¾
