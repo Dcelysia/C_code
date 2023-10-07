e300ba319ffdee8d76f3b2034ab5f869d37af6b9
Elysia <6873058@qq.com>
2023-10-07 11:25:21 +0800

    daily practice

---
+++ Openjudge-10-6/practice.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdbool.h>
+#include <stdio.h>
+
+//int main(void)
+//{
+//	int n1 = 0;
+//	short s1 = 0;
+//	printf("%d %d", sizeof(n1), sizeof(s1));
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	float f1 = 0;
+//	double d1 = 0;
+//	printf("%d %d", sizeof(f1), sizeof(d1));
+//	return 0;
+//}
+//int main(void)
+//{
+//	bool f1 = 0;
+//	char d1 = 0;
+//	printf("%d %d", sizeof(f1), sizeof(d1));
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	printf("D C");
+//	return 0;
+//}
+//int main(void)
+//{
+//	double a;
+//	float b;
+//	a = 1.000000001;
+//	b = a;
+//	printf("%.9f %.9f\n", a, b);
+//	return 0;
+//}
+
+int main(void)
+{
+	printf("F E");
+	return 0;
+}
