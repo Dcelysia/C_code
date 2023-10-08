96cfb2779d24cee1572783b7454080902215315a
Elysia <6873058@qq.com>
2023-10-08 21:34:50 +0800

    daily practice

---
+++ Openjudge-10-7/practice.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+//int main(void)
+//{
+//	float ip1 = 0;
+//	scanf("%f", &ip1);
+//	int np1 = (int)ip1;
+//	printf("%d", np1);
+//	return 0;
+//}
+//
+//int main(void)
+//{
+//	char ip1 = 0;
+//	scanf("%c", &ip1);
+//	printf("%d", ip1);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int ip1 = 0;
+//	scanf("%d", &ip1);
+//	printf("%c", ip1);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int ip1 = 0;
+//	scanf("%d", &ip1);
+//	bool op = ip1;
+//	printf("%d",op);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	char ip[] = "Hello, World!";
+//	int op2 = sizeof(ip);
+//	printf("%d",op2);
+//	return 0;
+//}
