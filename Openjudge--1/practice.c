f3a7d2bd76e6fae602d9696287b89f752b60231b
Elysia <6873058@qq.com>
2023-10-15 07:52:59 +0800

    daily practice

---
+++ Openjudge--1/practice.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main(void)
+//{
+//	int ip1 = 0, ip2 = 0;
+//	scanf("%d %d", &ip1, &ip2);
+//	printf("%d", ip1 + ip2);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int n1 = 0, n2 = 0, n3 = 0;
+//	scanf("%d %d %d", &n1, &n2, &n3);
+//	printf("%d", (n1 + n2) * n3);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int n1 = 0, n2 = 0, n3 = 0;
+//	scanf("%d %d %d", &n1, &n2, &n3);
+//	printf("%d", (n1 + n2) / n3);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int ip1 = 0, ip2 = 0;
+//	scanf("%d %d", &ip1, &ip2);
+//	printf("%d %d", ip1 / ip2, ip1 % ip2);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	double ip1 = 0, ip2 = 0;
+//	scanf("%lf %lf", &ip1, &ip2);
+//	double op1 = 0;
+//	op1 = ip1 / ip2;
+//	printf("%.9lf", op1);
+//	return 0;
+//}
